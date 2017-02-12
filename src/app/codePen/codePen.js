942696ad0a431c8f3cafbacacc936265ca50ad79
MKFMIKU <mikumkf@outlook.com>
2017-02-12 18:24:46 +0800

    Finished simple front page

--- src/app/codePen/codePen.js
+++ src/app/codePen/codePen.js
@@ -1,29 +1,37 @@
 import React, {Component} from 'react';
 
 import './codePen.styl';
-import 'codemirror/lib/codemirror.css';
 
 import CodeMirror from 'react-codemirror';
+import 'codemirror/lib/codemirror.css';
+
+import codeMirror from 'codemirror';
+import 'codemirror/addon/mode/loadmode';
+import 'codemirror/mode/meta';
 import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/xml/xml';
-import 'codemirror/mode/markdown/markdown';
 
 export class CodePen extends Component {
   render() {
     return (
       <div className="codePen">
         <div className="describe">
-          <input type="text" placeholder="Gist description"/>
+          <input type="text" placeholder="Gist description" value={this.state.description} onChange={this.handleDescription}/>
+        </div>
 
-          <div className="mainBoard">
-            <div className="fileHeader">
-              <div className="fileName">
-                <input type="text" placeholder="Filename with extension" value={this.state.fileName} onChange={this.handleFileName}/>
-              </div>
+        <div className="mainBoard">
+          <div className="fileHeader">
+            <div className="fileName">
+              <input type="text" placeholder="Filename with extension" value={this.state.fileName} onChange={this.handleFileName}/>
             </div>
-            <div className="fileContent">
-              <CodeMirror name="CodeMirror" onChange={this.handleCode} value={this.state.code} options={this.options}/>
-            </div>
+            <span>{this.state.mode}</span>
+          </div>
+          <div className="fileContent">
+            <CodeMirror
+              name="CodeMirror"
+              onChange={this.handleCode}
+              value={this.state.code}
+              options={this.options}
+              />
           </div>
         </div>
       </div>
@@ -34,8 +42,9 @@ export class CodePen extends Component {
     this.state = {
       fileName: "",
       code: "",
-      readOnly: false,
-      mode: 'javascript'
+      description: "",
+      mode: 'text/javascript',
+      readOnly: false
     };
     this.options = {
       lineNumbers: true,
@@ -43,11 +52,57 @@ export class CodePen extends Component {
     };
     this.handleCode = this.handleCode.bind(this);
     this.handleFileName = this.handleFileName.bind(this);
+    this.handleDescription = this.handleDescription.bind(this);
   }
   handleCode(newCode) {
     this.setState({code: newCode});
+    this.props.changeCodeObj({
+      fileName: this.state.fileName,
+      code: this.state.code,
+      description: this.state.description,
+      mode: this.state.mode
+    });
+  }
+  handleDescription(event) {
+    this.setState({description: event.target.value});
+    this.props.changeCodeObj({
+      fileName: this.state.fileName,
+      code: this.state.code,
+      description: this.state.description,
+      mode: this.state.mode
+    });
   }
   handleFileName(event) {
     this.setState({fileName: event.target.value});
+    let m;
+    let mode;
+    let spec;
+    if (/.+\.([^.]+)$/.exec(event.target.value)) {
+      m = /.+\.([^.]+)$/.exec(event.target.value);
+      const info = codeMirror.findModeByExtension(m[1]);
+      if (info) {
+        mode = info.mode;
+        spec = info.mime;
+      }
+    } else if (/\//.test(event.target.value)) {
+      const info = codeMirror.findModeByMIME(event.target.value);
+      if (info) {
+        mode = info.mode;
+        spec = event.target.value;
+      }
+    }
+    if (mode) {
+      this.setState({mode: spec});
+    }
+    this.props.changeCodeObj({
+      fileName: this.state.fileName,
+      code: this.state.code,
+      description: this.state.description,
+      mode: this.state.mode
+    });
   }
 }
+
+CodePen.propTypes = {
+  changeCodeObj: React.PropTypes.func
+};
--- src/app/home.js
+++ src/app/home.js
@@ -4,12 +4,40 @@ import {Header} from './header/header';
 import {CodePen} from './codePen/codePen';
 
 export class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      CodeObj: {
+        fileName: "",
+        code: "",
+        mode: "",
+        description: ""
+      }
+    };
+    this.changeCodeObj = this.changeCodeObj.bind(this);
+    this.handleCreateNew = this.handleCreateNew.bind(this);
+  }
+  changeCodeObj(newCodeObj) {
+    this.setState({CodeObj: newCodeObj});
+  }
+  handleCreateNew() {
+    fetch('/new', {
+      method: "POST",
+      body: JSON.stringify(this.state.CodeObj)
+    }).then(res => console.log(res), err => console.log(err));
+  }
   render() {
     return (
       <div className="container">
         <Header name="Header"/>
 
-        <CodePen name="CodePen"/>
+        <CodePen name="CodePen" changeCodeObj={this.changeCodeObj}/>
+
+        <div className="control">
+          <button className="submit" onClick={this.handleCreateNew}>Create</button>
+        </div>
+
       </div>
     );
   }
